9a89c74d8a6a94322082e46ff731e789ade37995
kawaibird <104888211+kawaibird@users.noreply.github.com>
2023-12-15 18:03:47 +0900

    bugfix

--- main.ts
+++ main.ts
@@ -1,17 +1,35 @@
-import { App, Plugin, SuggestModal, TFile } from 'obsidian';
+import { App, Notice, Plugin, SuggestModal, TAbstractFile, TFile, TFolder } from 'obsidian';
+import { buffer } from 'stream/consumers';
+
+function _recur_subdir_read(folder: TFolder, ret: string[]) {
+	folder.children.forEach(file => {
+		if (file instanceof TFolder) {
+			ret.push(file.path);
+			_recur_subdir_read(file, ret);
+		}
+	});
+}
+
+function _recur_subfiles_read(folder: TFolder, ret: TFile[]) {
+	folder.children.forEach(file => {
+		if (file instanceof TFolder) {
+			_recur_subfiles_read(file, ret);
+		}
+		else if (file instanceof TFile) {
+			ret.push(file);
+		}
+	});
+}
 
 export class SimpleDictionaryModal extends SuggestModal<string> {
 	app: App;
-	sorted_files: TFile[];
 
 	getSuggestions(query: string): string[] | Promise<string[]> {
-		const files = this.app.vault.getFiles();
-		const sorted_files = files.sort((a, b) => {
-			const a_is_smaller = a.basename < b.basename;
-			return a_is_smaller ? -1 : 1;
-		});
-		this.sorted_files = sorted_files;
-		const folder_names = sorted_files.map(file => { return file.parent?.path; }).unique();
+		const root_folder = this.app.vault.getRoot();
+		let folder_names: string[] = [];
+		_recur_subdir_read(root_folder, folder_names);
+		folder_names.unique();
+		folder_names.push('/');
 
 		return folder_names;
 	}
@@ -26,15 +44,17 @@ export class SimpleDictionaryModal extends SuggestModal<string> {
 			condition = item + '/';
 		}
 
-		const targets = this.sorted_files.filter(file => {
-			return file.path.startsWith(condition);
+		let files: TFile[] = [];
+		const cur_path: TFolder = this.app.vault.getAbstractFileByPath(item);
+		_recur_subfiles_read(cur_path, files);
+		files.sort((a, b) => {
+			let cond = a.basename < b.basename ? -1 : 1;
+			return cond;
 		});
 
-		const contents = targets.map(file => { return `[${file.basename}](${file.path})<br>`; })
-			.reduce((accumulator, cur_name) => accumulator + cur_name, '');
-
-		let editor = this.app.workspace.activeEditor?.editor;
-		editor?.replaceRange(contents, editor?.getCursor());
+		files.forEach(file => {
+			this.app.workspace.activeEditor?.editor?.replaceSelection(`[${file.basename}: ${file.parent?.name}](${file.path})<br>`);
+		});
 	}
 }
 
